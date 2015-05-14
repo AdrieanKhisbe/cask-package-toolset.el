3100961b44c59ea6c456c512231eb53f106d51ed
Adriean Khisbe <adriean.khisbe@live.fr>
2015-05-14 21:05:48 +0200

    dev: add scaffold ecukes files

---
+++ features/support/env.el
@@ -0,0 +1,32 @@
+(require 'f)
+
+(defvar cask-package-toolset-support-path
+  (f-dirname load-file-name))
+
+(defvar cask-package-toolset-features-path
+  (f-parent cask-package-toolset-support-path))
+
+(defvar cask-package-toolset-root-path
+  (f-parent cask-package-toolset-features-path))
+
+(add-to-list 'load-path cask-package-toolset-root-path)
+
+(require 'cask-package-toolset)
+(require 'espuds)
+(require 'ert)
+
+(Setup
+ ;; Before anything has run
+ )
+
+(Before
+ ;; Before each scenario is run
+ )
+
+(After
+ ;; After each scenario is run
+ )
+
+(Teardown
+ ;; After when everything has been run
+ )
