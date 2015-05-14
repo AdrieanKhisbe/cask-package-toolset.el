7d00003b03d58ce067a491a415414ff02c32e8b7
Adriean Khisbe <adriean.khisbe@live.fr>
2015-05-14 11:09:33 +0200

    init: .el skeleton

---
+++ cask-package-toolset.el
@@ -0,0 +1,32 @@
+;;; cask-package-toolset.el --- Toolsettize your package
+
+;; Copyright (C) 2015  Adrien Becchis
+
+;; Author: Adrien Becchis <adriean.khisbe@live.fr>
+;; Created:  2015-05-14
+;; Version: 0.1
+;; Keywords: convenience, tools
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+
+;;; Code:
+
+
+(provide 'cask-package-toolset)
+;;; cask-package-toolset.el ends here
