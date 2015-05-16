3487c136c34f7bf9e94096149b73ab10d28c9f7a
Adriean Khisbe <adriean.khisbe@live.fr>
2015-05-16 09:47:43 +0200

    fix: malencontrous Copy that end up in M-c

--- cask-package-toolset.el
+++ cask-package-toolset.el
@@ -4,10 +4,10 @@
 
 ;; Author: Adrien Becchis <adriean.khisbe@live.fr>
 ;; Created:  2015-05-14
-;; Version: 0.2.1
+;; Version: 0.2.2
 ;; Keywords: convenience, tools
 ;; Url: http://github.com/AdrieanKhisbe/cask-package-toolset.el
-;; Package-Requires: ((emacs "24") (cl-lib "0.3") (S "1.6.1") (dash "1.8.0") (f "0.10.0") (commander "0.2.0") (ansi "0.1.0") (shut-up "0.1.0") (magit "1.4.0"))
+;; Package-Requires: ((emacs "24") (cl-lib "0.3") (s "1.6.1") (dash "1.8.0") (f "0.10.0") (commander "0.2.0") (ansi "0.1.0") (shut-up "0.1.0") (magit "1.4.0"))
 
 ;; This file is not part of GNU Emacs.
 
