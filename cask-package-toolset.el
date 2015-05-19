319da16e8e5ddca4fe2276cbdf9eea04643e42fe
Adriean Khisbe <adriean.khisbe@live.fr>
2015-05-19 11:09:54 +0200

    fix: replace cadr, caddr with nth (rather to replace with cl-cadddr, and co)

--- cask-package-toolset.el
+++ cask-package-toolset.el
@@ -4,7 +4,7 @@
 
 ;; Author: Adrien Becchis <adriean.khisbe@live.fr>
 ;; Created:  2015-05-14
-;; Version: 0.4
+;; Version: 0.4.1
 ;; Keywords: convenience, tools
 ;; Url: http://github.com/AdrieanKhisbe/cask-package-toolset.el
 ;; Package-Requires: ((emacs "24") (cl-lib "0.3") (s "1.6.1") (dash "1.8.0") (f "0.10.0") (commander "0.2.0") (ansi "0.1.0") (shut-up "0.1.0") (magit "1.4.0"))
@@ -228,7 +228,7 @@ Note it remove enventual trailing .el"
            )))
     (-each file-status
       (lambda (it)
-        (message "- %s →   %s" (s-pad-right 28 " " (ansi-blue (car it))) (if (cadr it) (ansi-green "Ok") (ansi-red (caddr it))))))))
+        (message "- %s →   %s" (s-pad-right 28 " " (ansi-blue (car it))) (if (nth 1 it) (ansi-green "Ok") (ansi-red (nth 2))))))))
 
 (commander
  (name "cask-package-toolset")
