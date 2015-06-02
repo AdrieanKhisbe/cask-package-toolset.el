2f1a067fc649359d7299bb9f0b9235e4a0c7b1dd
Adriean Khisbe <adriean.khisbe@live.fr>
2015-06-02 10:04:20 +0200

    tt: add templates (for template-test)

---
+++ templates/test/package-test.el
@@ -0,0 +1,10 @@
+;;; Test for `${package-name}'
+
+;;; Commentary:
+;; These are the tests for `${package-name}'
+
+;;; Code:
+
+(ert-deftest ${package-name}-should-not-pass ()
+  (should-not nil))
+
---
+++ templates/test/test-helper.el
@@ -0,0 +1,38 @@
+;;; test-helper --- Test helper for ${package-name}
+
+;;; Commentary:
+;; test helper inspired from https://github.com/tonini/overseer.el/blob/master/test/test-helper.el
+
+;;; Code:
+
+(require 'f)
+
+(defvar cpt-path
+  (f-parent (f-this-file)))
+
+(defvar ${package-name}-test-path
+  (f-dirname (f-this-file)))
+
+(defvar ${package-name}-root-path
+  (f-parent ${package-name}-test-path))
+
+(defvar ${package-name}-sandbox-path
+  (f-expand "sandbox" ${package-name}-test-path))
+
+(when (f-exists? ${package-name}-sandbox-path)
+  (error "Something is already in %s. Check and destroy it yourself" ${package-name}-sandbox-path))
+
+(defmacro within-sandbox (&rest body)
+  "Evaluate BODY in an empty sandbox directory."
+  `(let ((default-directory ${package-name}-sandbox-path))
+     (when (f-exists? ${package-name}-sandbox-path)
+       (f-delete default-directory :force))
+     (f-mkdir ${package-name}-sandbox-path)
+     ,@body
+     (f-delete default-directory :force)))
+
+(require 'ert)
+(require '${package-name} (f-expand "${package-name}" ${package-name}-root-path))
+
+(provide 'test-helper)
+;;; test-helper.el ends here
