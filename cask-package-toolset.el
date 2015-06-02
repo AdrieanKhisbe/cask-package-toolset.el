f666125d57fafb52b86ceef6bc6ae3879a05a4a4
Adriean Khisbe <adriean.khisbe@live.fr>
2015-06-02 12:14:25 +0200

    feature: can generate real ert scaffold

--- cask-package-toolset.el
+++ cask-package-toolset.el
@@ -4,7 +4,7 @@
 
 ;; Author: Adrien Becchis <adriean.khisbe@live.fr>
 ;; Created:  2015-05-14
-;; Version: 0.4.3
+;; Version: 0.5
 ;; Keywords: convenience, tools
 ;; Url: http://github.com/AdrieanKhisbe/cask-package-toolset.el
 ;; Package-Requires: ((emacs "24") (cl-lib "0.3") (s "1.6.1") (dash "1.8.0") (f "0.10.0") (commander "0.2.0") (ansi "0.1.0") (shut-up "0.1.0") (magit "1.4.0"))
@@ -280,7 +280,7 @@ Note it remove enventual trailing .el"
            ,@(-map (lambda (file)(list file (f-exists? file) "You should run `package-toolset'"))
                    cask-package-toolset-templates)
            ;; check installed ert
-           ("Ert Test" ,(f-directory? "test") "You should run `cask exec ert-runner init'")
+           ("Ert Test" ,(f-directory? "test") "You should run `cask exec package-toolset setup-ert'")
            ;; check installed ecukes
            ("Ecukes Features" ,(and (f-directory? "features/step-definitions")
                                     (f-directory? "features/support"))
