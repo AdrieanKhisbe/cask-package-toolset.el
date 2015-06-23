626e60d14cbdc5990fe1806b6a781ec301ca6e35
Adriean Khisbe <adriean.khisbe@live.fr>
2015-06-23 08:01:19 +0200

    coverage: update README and bump version

--- cask-package-toolset.el
+++ cask-package-toolset.el
@@ -4,7 +4,7 @@
 
 ;; Author: Adrien Becchis <adriean.khisbe@live.fr>
 ;; Created:  2015-05-14
-;; Version: 0.5.1
+;; Version: 0.6
 ;; Keywords: convenience, tools
 ;; Url: http://github.com/AdrieanKhisbe/cask-package-toolset.el
 ;; Package-Requires: ((emacs "24") (cl-lib "0.3") (s "1.6.1") (dash "1.8.0") (f "0.10.0") (commander "0.2.0") (ansi "0.1.0") (shut-up "0.1.0") (magit "1.4.0"))
