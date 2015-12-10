bc8d020c407ccdfbc26a1060dd1383e1e098a81d
AdrieanKhisbe <adriean.khisbe@live.fr>
2015-12-10 22:21:45 +0100

    Update CHANGELOG and README about update-travis

--- cask-package-toolset.el
+++ cask-package-toolset.el
@@ -4,7 +4,7 @@
 
 ;; Author: Adrien Becchis <adriean.khisbe@live.fr>
 ;; Created:  2015-05-14
-;; Version: 0.6.6
+;; Version: 0.7.0
 ;; Keywords: convenience, tools
 ;; Url: http://github.com/AdrieanKhisbe/cask-package-toolset.el
 ;; Package-Requires: ((emacs "24") (cl-lib "0.3") (s "1.6.1") (dash "1.8.0") (f "0.10.0") (commander "0.2.0") (ansi "0.1.0") (shut-up "0.1.0"))
@@ -309,7 +309,7 @@ Note it remove enventual trailing .el"
                 (destination-file (f-expand ".travis.yml")))
             (message "Updating travis config\nYou might need to check the emacs version you want to support")
             (f-delete destination-file)
-            (f-copy template-file destination-file) (message "bite"))))
+            (f-copy template-file destination-file))))
     (error "Travis not setup yet, run `setup' instead")))
 
 (defun cask-package-toolset-print-badges ()
